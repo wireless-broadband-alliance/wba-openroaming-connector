2b3040768dc417c0d7235233cbe6617400e50657
Nuno Facha <nunofacha@gmail.com>
2025-07-22 12:50:24 +0000

    Rename repo

--- anp/prepare-debian11.sh
+++ anp/prepare-debian11.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This script is meant for quick & easy install via:
-#   $ curl -fsSL https://raw.githubusercontent.com/wireless-broadband-alliance/openroaming-oss/main/prepare-debian11.sh -o prepare-debian11.sh
+#   $ curl -fsSL https://raw.githubusercontent.com/wireless-broadband-alliance/wba-openroaming-connector/main/prepare-debian11.sh -o prepare-debian11.sh
 #   $ chmod +x prepare-debian11.sh
 #   $ ./prepare-debian11.sh
 
-REPO_URL="https://github.com/wireless-broadband-alliance/openroaming-oss.git"
-CERTS_PATH="/root/openroaming-oss/certs"
+REPO_URL="https://github.com/wireless-broadband-alliance/wba-openroaming-connector.git"
+CERTS_PATH="/root/wba-openroaming-connector/certs"
 
 if [ "$EUID" -ne 0 ]
   then echo "You must run this script as root, you can either sudo the script directly or become root with a command such as 'sudo su'"
@@ -48,19 +48,19 @@ pip3 install docker-compose
 cd /root
 git clone $REPO_URL
 # Prepare certificates
-cd /root/openroaming-oss/anp/configs/radsecproxy/certs/chain
-rm -rf /root/openroaming-oss/anp/configs/radsecproxy/certs/key.pem
-rm -rf /root/openroaming-oss/anp/configs/radsecproxy/certs/client.pem
-rm -rf /root/openroaming-oss/anp/configs/radsecproxy/certs/chain.pem
+cd /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/chain
+rm -rf /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/key.pem
+rm -rf /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/client.pem
+rm -rf /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/chain.pem
 #Prepare RadSec Certs
-cp $CERTS_PATH/wba/key.pem /root/openroaming-oss/anp/configs/radsecproxy/certs/key.pem
-cp $CERTS_PATH/wba/client.pem /root/openroaming-oss/anp/configs/radsecproxy/certs/client.pem
-cat /root/openroaming-oss/anp/configs/radsecproxy/certs/client.pem /root/openroaming-oss/anp/configs/radsecproxy/certs/chain/WBA_Issuing_CA.pem /root/openroaming-oss/anp/configs/radsecproxy/certs/chain/WBA_Cisco_Policy_CA.pem /root/openroaming-oss/anp/configs/radsecproxy/certs/chain/WBA_Issuing7_CA.pem /root/openroaming-oss/anp/configs/radsecproxy/certs/chain/WBA_Policy7_CA.pem > /root/openroaming-oss/anp/configs/radsecproxy/certs/chain.pem
-sed -i "s/-RNAME-/${realm_name//./\\.}/g" /root/openroaming-oss/anp/configs/radsecproxy/radsecproxy.conf
-sed -i "s|-RCLIENT-|${client_cidr}|g" /root/openroaming-oss/anp/configs/radsecproxy/radsecproxy.conf
-sed -i "s/-RSECRET-/${client_secret}/g" /root/openroaming-oss/anp/configs/radsecproxy/radsecproxy.conf
+cp $CERTS_PATH/wba/key.pem /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/key.pem
+cp $CERTS_PATH/wba/client.pem /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/client.pem
+cat /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/client.pem /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/chain/WBA_Issuing_CA.pem /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/chain/WBA_Cisco_Policy_CA.pem /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/chain/WBA_Issuing7_CA.pem /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/chain/WBA_Policy7_CA.pem > /root/wba-openroaming-connector/anp/configs/radsecproxy/certs/chain.pem
+sed -i "s/-RNAME-/${realm_name//./\\.}/g" /root/wba-openroaming-connector/anp/configs/radsecproxy/radsecproxy.conf
+sed -i "s|-RCLIENT-|${client_cidr}|g" /root/wba-openroaming-connector/anp/configs/radsecproxy/radsecproxy.conf
+sed -i "s/-RSECRET-/${client_secret}/g" /root/wba-openroaming-connector/anp/configs/radsecproxy/radsecproxy.conf
 # ready workdir
-cd /root/openroaming-oss/anp/
+cd /root/wba-openroaming-connector/anp/
 docker-compose up -d
 
 echo "Reminder: Make sure UDP ports 11812 and 11813 are open on your firewall (on your cloud provider if applicable), refer to the documentation for more details"
